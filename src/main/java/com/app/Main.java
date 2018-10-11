ff2b9e8873a088dfeaf90c7e9662d2f0fe12c71e
Jaras49 <jaras48@gmail.com>
2018-10-11 21:53:27 +0200

    Main class added

---
+++ src/main/java/com/app/Main.java
@@ -0,0 +1,11 @@
+package com.app;
+
+import com.app.data.input.FileToObjectReader;
+import com.app.data.storage.InMemoryDataStorage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new MovieApp(new InMemoryDataStorage(), new FileToObjectReader()).run();
+    }
+}
