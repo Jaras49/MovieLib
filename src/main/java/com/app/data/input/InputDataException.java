e4d954f7a302e59b1df1ab72cc58007ad646ce18
Jaras49 <jaras48@gmail.com>
2018-10-11 21:43:37 +0200

    added public access modifier

--- src/main/java/com/app/data/input/InputDataException.java
+++ src/main/java/com/app/data/input/InputDataException.java
@@ -1,6 +1,6 @@
 package com.app.data.input;
 
-class InputDataException extends Exception {
+public class InputDataException extends Exception {
 
     public static final String UNSUPPORTED_FILE_FORMAT = "Unsupported file format";
     public static final String MALFORMED_DATA = "Invalid Json/Xml form";
